6fd4bb7a686046e2560a4e78fbc2f369654b6ba8
korir93 <53041618+korir93@users.noreply.github.com>
2019-10-14 16:00:44 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html>
+		<head>
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+			<title>BRI-Tech</title>
+			<link rel="stylesheet" type="text/css" href="assets/css/app.css"/>
+			<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css"/>
+			<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css"/>
+		</head>
+<body>
+ 		<!-- nav header ---->
+			<div class="fixed-top">
+			  <header class="topbar">
+			      <div class="container">
+			        <div class="row">
+			          <!-- social icon-->
+			          <div class="col-sm-12">
+			            <ul class="social-network">
+			              <li><a class="waves-effect waves-dark" href="tel:0719871091"><i class="fa fa-phone"></i></a></li>
+				            <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-facebook"></i></a></li>
+			              <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-twitter"></i></a></li>
+			              <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-linkedin"></i></a></li>
+			              <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-pinterest"></i></a></li>
+			              <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-google-plus"></i></a></li>
+			            </ul>
+			          </div>
+
+			        </div>
+			      </div>
+			  </header>
+			  <nav class="navbar navbar-expand-lg navbar-dark mx-background-top-linear">
+			    <div class="container">
+			      <a class="navbar-brand" href="index.html" style="text-transform: uppercase;"> BRITECH Solutions</a>
+			      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+			        <span class="navbar-toggler-icon"></span>
+			      </button>
+			      <div class="collapse navbar-collapse" id="navbarResponsive">
+
+			        <ul class="navbar-nav ml-auto">
+
+			          <li class="nav-item active">
+			            <a class="nav-link" href="#carouselExampleFade">Home
+			              <span class="sr-only">(current)</span>
+			            </a>
+			          </li>
+
+			          <li class="nav-item">
+			            <a class="nav-link" href="#about">About</a>
+			          </li>
+
+			          <li class="nav-item">
+			            <a class="nav-link" href="#services">Services</a>
+			          </li>
+
+			          <li class="nav-item">
+			            <a class="nav-link" href="#contacts">Contact</a>
+			          </li>
+			        </ul>
+			      </div>
+			    </div>
+			  </nav>
+			</div>
+		<!-- end nav header -->
+		<!-- beggin carousel -->
+		 	<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  				<div class="carousel-inner">
+				    <div class="carousel-item active">
+				      <img class="d-block w-100" src="assets/images/slide2.jpg" alt="Second slide">
+				          <div class="carousel-caption" style="transform:translateY(-70%)">
+								     	<h1>BRI-Tech</h1>
+											<h3> Solutions</h3>
+								   </div>
+
+				    </div>
+				    <div class="carousel-item">
+				      <img class="d-block w-100" src="assets/images/slide3.jpg" alt="Third slide">
+							<div class="carousel-caption" style="transform:translateY(-70%)">
+									<h1>Phone Repair</h1>
+									<h3>
+										All sorts of phone repairs e.g Screen repairs of all phones , phone flushing
+									</h3>
+							</div>
+				    </div>
+				 </div>
+			  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+			</div>
+		<!-- end carousel -->
+		<!-- jumbotron about us -->
+     <div class="container-fluid">
+         <div class="row jumbotron p-4">
+	         <div class="col-xs-12 col-sm-12 col-md-12 col-xl-12">
+		         <p class="lead text-center">
+			         	Britech Solutions Limited is a private company founded by <b>Brian</b> which is an IT firm dealing with <br>
+								<b>
+									<i class="fa fa-check"></i> Phone repairs
+									<i class="fa fa-check"></i> Unlocking
+									<i class="fa fa-check"></i> Rom Installations
+									<i class="fa fa-check"></i> Flushing
+									<i class="fa fa-check"></i> Computer repairs
+									<i class="fa fa-check"></i> Software installations
+								</b>
+								<br>We are located in <a href="#	" >Machakos town Opposite Naivas Supermarket, Stall no 7</a>..
+		         </p>
+	         </div>
+         </div>
+         </div>
+     </div>
+		 <!--	end jumbotron	-->
+     <!-- welcome section -->
+     <div class="container  paddding" id="about">
+		     	<div class="welcome ">
+			     		<h1 class="text-center text-dark" >About us</h1>
+							<h5 class="text-center" >We are dedicated to serve our clients to their satisfaction</h5>
+			    </div>
+		  		<hr class="separator" >
+			     <div class="col-12">
+				     <p class="lead">
+								We're passionate about clients; our curiosity and customer and consumer insights drive growth. We
+								care for our brands and are bold in pursuing their full potential. We can live this value through being
+								innovative, constantly searching for new ideas that drive growth and spinning them across the business.
+						 </p>
+				 	 </div>
+					 <div class=" ">
+				     	 <div class="p-2">
+					    </div>
+							 <div class="row pl-3">
+						 	<div class="col-lg-2 col-md-2 col-sm-1 col-xs-0"></div>
+						 	<div class="col-lg-9 col-md-9 col-sm-10 col-xs-12 row p-3">
+								 	<div class="col-lg-3 col-md-3 col-sm-5 col-xs-12">
+	 					     		<h5 class="text-uppercase text-center" >Brian (Brayo)</h5>
+										<img src="assets/images/brayo.jpg"  class="img-fluid img-thumbnail img-rounded"/>
+										<h5>Founder and CEO</h5>
+								 	</div>
+		 						 	<div class="col-lg-9 col-md-9 col-sm-7 col-xs-12">
+											<h5 class="text-danger"  >About me </h5>
+											<p class="lead" >
+												Hi there I am Brian the founder and CEO of Bri-Tech Solutions.
+											</p>
+											<h5 class="text-danger" >Why work with me </h5>
+											<p class="lead" >
+												I have experience working in the mobile phone industry and i am oriented on service delivery to my clients
+											</p>
+		 						 	</div>
+							</div>
+					 </div>
+					</div>
+		</div>
+		 <!-- end welcome section  -->
+ 		<hr class="separator" >
+		 <!-- services section-->
+		 <div class="container padding" id="services">
+		      <div class="welcome text-center">
+			     		<h1 >Our Services</h1>
+					</div>
+			     <div class="col-12">
+				     <p class="lead text-center">
+								This are some the services we offer at very affordable rates
+						 </p>
+				 	 </div>
+		     	<div class="container padding">
+							<div class="row padding"  >
+
+								<div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 my-2">
+									<div class="card">
+										<img src="assets/images/service1.jpg" style="width:100%; height:200px" class="card-img-top"/>
+										<div class="card-body">
+											<h4 class="card-title">Repair & Servicing</h4>
+											<p class="card-text">We repair all sorts of mobile phones. Any kind of damages e.g<br>
+											Screen replacements , Phone flushing </p>
+										</div>
+									</div>
+								</div>
+
+								<div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 my-2">
+									<div class="card">
+										<img src="assets/images/service2.jpg" style="width:100%; height:200px" class="card-img-top">
+										<div class="card-body">
+												<h4 class="card-title">Flushing & Unlocking</h4>
+												<p class="card-text">We offer flusing, unlocking services off all kinds of phones <br><b class="text-uppercase" >android , windows , iphones</b> ..<br>
+												</p>
+								    </div>
+									</div>
+								</div>
+
+
+								<div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 my-2">
+									<div class="card">
+										<img src="assets/images/slide2.jpg" style="width:100%; height:200px" class="card-img-top">
+										<div class="card-body">
+												<h4 class="card-title">Phones & Accessories</h4>
+												<p class="card-text">We have a stock of all latest phones & their accesories e.g<br>
+												Screen guards , Back covers </p>
+										</div>
+									</div>
+								</div>
+
+
+							</div>
+						</div>
+					</div>
+			</div>
+		 <!-- end services section-->
+		 <!-- start footer -->
+			<footer class="p-3" style="background-color:#3f3f3f ;color:#d5d5d5" id="contacts">
+ 		     <!-- <div class="welcome text-center">
+ 			     <div class="p-3">
+ 			     		<h4 class="text-center" >Contacts</h4>
+ 			     </div>
+				 </div> -->
+					<div class="container-fluid">
+					    <div class="row">
+					        <div class="col-md-4 col-xs-12 col-sm -12 col-lg-4">
+					            <h3>Bri-Tech Solutions</h3>
+					            <hr class="light1">
+					            <p> <i class="fa fa-map-marker"> </i>&nbsp;&nbsp;  Machakos Town,
+												<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Opposite Naivas Supermarket ,<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stall no 7</p>
+					        </div>
+					        <div class="col-md-4  col-xs-12 col-sm -12 col-lg-4">
+					            <p>  <i class="fa fa-th-list"> </i>&nbsp;&nbsp; Our Services</p>
+					            <hr class="light1">
+											<ul>
+						            <li><i class="fa fa-circle iconsmall"> </i> Mobile Phones Repair</li>
+						            <li><i class="fa fa-circle iconsmall"> </i> Phones Flushing</li>
+						            <li><i class="fa fa-circle iconsmall"> </i> Phone Accesories</li>
+											</ul>
+					        </div>
+					        <div class="col-md-4   col-xs-12 col-sm -12 col-lg-4">
+											<p> <i class="fa fa-phone"> </i>&nbsp;&nbsp;Contact Us</p>
+					            <hr class="light1">
+					            <p><a  class="text-light" href="mailto:info@britech.co.ke"> <i class="fa fa-envelope"> </i>&nbsp;&nbsp;info@britech.co.ke</a><p>
+					            <p ><a  class="text-light" href="tel:0719871091"> <i class="fa fa-phone"> </i>&nbsp;&nbsp;0719 871 091</a></p>
+					        </div>
+					    </div>
+					</div>
+			</footer>
+ 			<div class="text-center bg-dark p-2">
+ 				<p class="text-light">Copyright &copy <?= date('Y');?> | All Rights Reserved |<br> www.britech.com</p>
+ 		 </div>
+		 <!-- end footer -->
+</body>
+</html>
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.js"></script>
